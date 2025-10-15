43070f1fb8bb02c975def946e85875c97beba14b
DiegzM <147036619+DiegzM@users.noreply.github.com>
2025-10-14 18:02:20 -0700

    Remove pieces from View

--- src/main/java/pyraminx/model/PyraminxModel.java
+++ src/main/java/pyraminx/model/PyraminxModel.java
@@ -5,22 +5,46 @@ package pyraminx.model;
  *
  *
  */
+
 public class PyraminxModel {
-    private Piece[] pieces;
+
+    // Constants
+    public static final int TIP_COUNT = 4;
+    public static final int EDGE_COUNT = 6;
+    public static final int CENTER_COUNT = 4;
+
+    // Tip Constants
+    public static final int TIP_TOP = 0;
+    public static final int TIP_LEFT = 1;
+    public static final int TIP_RIGHT = 2;
+    public static final int TIP_BACK = 3;
+
+    // Edge Constants
+    public static final int EDGE_FRONT_LEFT    = 0; 
+    public static final int EDGE_FRONT_RIGHT   = 1; 
+    public static final int EDGE_FRONT_BOTTOM  = 2; 
+    public static final int EDGE_RIGHT_BOTTOM  = 3; 
+    public static final int EDGE_BACK_BOTTOM   = 4; 
+    public static final int EDGE_LEFT_BOTTOM   = 5;
+
+    // Center Constants
+    public static final int CENTER_TOP = 0;
+    public static final int CENTER_LEFT = 1;
+    public static final int CENTER_RIGHT = 2;
+    public static final int CENTER_BACK = 3;
+
+    // Fields
+    private Tip[] tips;
+    private Edge[] edges;
+    private Center[] centers;
     private int[] orientations;
     
     public PyraminxModel() {
-        pieces = new Piece[14];
+        tips = new Tip[4];
+        edges = new Edge[6];
+        centers = new Center[4];
     }
     private void initializePieces() {
-        // Tip Pieces
-        pieces[0] = new Tip(0, new PColor[]{PColor.RED,  PColor.BLUE, PColor.YELLOW}); // TOP TIP
-        pieces[1] = new Tip(1,  new PColor[]{PColor.RED, PColor.YELLOW, PColor.GREEN}); // FRONT LEFT TIP
-        pieces[2] = new Tip(2,  new PColor[]{PColor.RED, PColor.GREEN, PColor.BLUE}); // FRONT RIGHT TIP
-        pieces[3] = new Tip(3,  new PColor[]{PColor.GREEN, PColor.YELLOW, PColor.BLUE}); // BACK TIP
-
-        // Edge Pieces
-        // Center Pieces
-
+        
     }
 }
