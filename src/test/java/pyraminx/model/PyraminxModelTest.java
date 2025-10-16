b76ef062bfdc3bd47c22ffaa7ae6d159ddf3dbc4
Cha <57724873+chablades@users.noreply.github.com>
2025-10-16 16:09:15 -0700

    Added:
    PyraminxModel:
    initializePieces()
    Correct face cycles
    applyOrientatiuon
    rotateFace // rotates piece
    rotate // helps rotateFace
    isSolved() //To check if the pyraminx is solved
    printState() //For testing

--- src/test/java/pyraminx/model/PyraminxModelTest.java
+++ src/test/java/pyraminx/model/PyraminxModelTest.java
@@ -22,6 +22,7 @@ class PyraminxModelTest {
             assertFalse(model.isSolved(), "After 2 turns, puzzle should not be solved");
             model.rotateFace(face, true);
             assertTrue(model.isSolved(), "After 3 turns, puzzle should be solved again");
+            model.printState();
         }
     }
 
@@ -31,6 +32,7 @@ class PyraminxModelTest {
         model.rotateFace(PyraminxModel.Face.TOP, true);
         model.rotateFace(PyraminxModel.Face.TOP, false);
         assertTrue(model.isSolved(), "A clockwise + counterclockwise turn should cancel out");
+        model.printState();
     }
 
     @Test
@@ -39,5 +41,39 @@ class PyraminxModelTest {
         model.rotateFace(PyraminxModel.Face.TOP, true);
         model.rotateFace(PyraminxModel.Face.LEFT, true);
         assertFalse(model.isSolved(), "Two different face turns should scramble the puzzle");
+        model.printState();
     }
+
+    @Test
+    void testFourTurnsEquivalentToOneCounterClockwise() {
+        for (PyraminxModel.Face face : PyraminxModel.Face.values()) {
+            PyraminxModel model1 = new PyraminxModel();
+            PyraminxModel model2 = new PyraminxModel();
+
+            // 4 clockwise turns
+            for (int i = 0; i < 4; i++) {
+                model1.rotateFace(face, true);
+            }
+
+            // 1 counterclockwise turn
+            model2.rotateFace(face, false);
+
+            assertEquals(model1.isSolved(), model2.isSolved(),
+                    "Four clockwise turns of " + face + " should equal one counterclockwise turn");
+        }
+    }
+
+    @Test
+    void testFullCycleOfAllFaces() {
+        PyraminxModel model = new PyraminxModel();
+        // Rotate each face 3 times (full cycle)
+        for (PyraminxModel.Face face : PyraminxModel.Face.values()) {
+            for (int i = 0; i < 3; i++) {
+                model.rotateFace(face, true);
+            }
+        }
+        assertTrue(model.isSolved(), "Three turns of every face should return to solved");
+    }
+
+
 }
